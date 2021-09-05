68d4fe8aee1506c316baa2821466d4bb138f1372
Jonas Riedel <jonasriedel@gmx.net>
2021-09-05 09:58:29 +0200

    Print info, advanced switch case statement

--- src/com/jonasriedel/Main.java
+++ src/com/jonasriedel/Main.java
@@ -22,18 +22,18 @@ public class Main {
 
         language = sc.nextLine();
 
-        switch(language) {
-            case "Java":
+        switch (language) {
+            case "Java" -> {
                 CreateFile.create(".gitignore_java");
                 WriteFile.writeFile(".gitignore_java", Java.generateJava());
-                break;
-            case "Javascript":
+                Main.printGeneratedInfo();
+            }
+            case "Javascript" -> {
                 CreateFile.create(".gitignore_javascript");
                 WriteFile.writeFile(".gitignore_javascript", Javascript.generateJavascript());
-                break;
-            default:
-                System.out.println("Not a valid language! Try again.");
-                break;
+                Main.printGeneratedInfo();
+            }
+            default -> System.out.println("Not a valid language! Try again.");
         }
     }
 
@@ -45,4 +45,7 @@ public class Main {
         System.out.println("Which language? Type 'help' for a list of all languages.");
     }
 
+    private static void printGeneratedInfo() {
+        System.out.println(".gitignore generated!");
+    }
 }
